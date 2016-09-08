b89b835439740a35b3ddca34589a3544f542af9f
Indraniel Das <idas@genome.wustl.edu>
2016-09-08 15:28:55 -0500

    + modify 1000G annotations
      - use a modified annotations file that has the population AF prefixed
        with "1KG".  This will distingush 1000G population AF
      - include population annotations AF in the INFO field

--- yaps2/resources/postvqsr/annotate-w-1000G.sh
+++ yaps2/resources/postvqsr/annotate-w-1000G.sh
@@ -3,14 +3,19 @@
 BCFTOOLS=/gsc/bin/bcftools1.2
 TABIX=/gsc/bin/tabix
 
-BIO_1662=/gscmnt/gc2802/halllab/idas/jira/BIO-1662
+BIO_1984=/gscmnt/gc2802/halllab/idas/jira/BIO-1984
 
 # the 1000G annotation file
-KGVCF=${BIO_1662}/data/derived/FinnMetSeq-WGS/1000-regenerate-1000G-annotation-vcf/
-KGVCF+=ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.decompose.normalize.reheader.w_ids.vcf.gz
+KGVCF=${BIO_1984}/data/manual/create-1000G-reformatted-af-annotations/
+KGVCF+=ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.decompose.normalize.reheader.w_ids.reformatted_pop_af.vcf.gz
 
 INVCF=$1
 OUTVCF=$2
 
-${BCFTOOLS} annotate -a ${KGVCF} -c ID -O z -o ${OUTVCF} ${INVCF} \
+${BCFTOOLS} annotate \
+    -a ${KGVCF} \
+    -c ID,INFO/1KG_EAS_AF,INFO/1KG_EUR_AF,INFO/1KG_AFR_AF,INFO/1KG_AMR_AF,INFO/1KG_SAS_AF \
+    -O z \
+    -o ${OUTVCF} \
+    ${INVCF} \
     && ${TABIX} -p vcf -f ${OUTVCF}
