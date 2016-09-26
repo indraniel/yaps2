bde7b3f03d080d5b864d6847a34dc550b957a568
Indraniel Das <idas@genome.wustl.edu>
2016-09-26 06:04:53 -0500

    + avoid grep giving an error when no variants match
      - based on this link:
        http://stackoverflow.com/questions/6550484/avoid-grep-returning-error-when-input-doesnt-match

--- yaps2/resources/mie/summarize_informative_var_only.sh
+++ yaps2/resources/mie/summarize_informative_var_only.sh
@@ -16,7 +16,7 @@ do
             OPT="-f .,PASS"
         fi
 
-        RESULT=$(${BCFTOOLS} view -s $IND,$DAD,$MOM $OPT $VCF | ${BCFTOOLS} view --min-ac 1 - | ${BCFTOOLS} view -s $DAD,$MOM - | ${BCFTOOLS} view -g hom - | grep -v 'SECONDARY' | grep -v '^#' | wc -l)
+        RESULT=$(${BCFTOOLS} view -s $IND,$DAD,$MOM $OPT $VCF | ${BCFTOOLS} view --min-ac 1 - | ${BCFTOOLS} view -s $DAD,$MOM - | ${BCFTOOLS} view -g hom - | grep -v 'SECONDARY' | { grep -v '^#' || true; } | wc -l)
         echo -e "$FAM\t$RESULT"
     fi
 done < $FAM_FILE
