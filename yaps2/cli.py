4f473cd0ea50da82bbb04787bb93c1286f55f8fb
Indraniel Das <idas@genome.wustl.edu>
2016-10-05 15:25:49 -0500

    + allow for both '-h' and '--help' to access help options

--- yaps2/cli.py
+++ yaps2/cli.py
@@ -5,7 +5,9 @@ import click
 
 from .version import __version__
 
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version=__version__)
 def cli():
     # to make this script/module behave nicely with unix pipes
