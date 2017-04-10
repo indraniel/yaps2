ebd59bdfd1ad2bf237b686fcc9702d4c8a36fa39
Indraniel Das <idas@genome.wustl.edu>
2017-04-10 13:32:49 -0500

    + handle case when input annotation vcf has no variants

--- yaps2/resources/postvqsr/annotate-w-1000G.sh
+++ yaps2/resources/postvqsr/annotate-w-1000G.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+ZCAT=/bin/zcat
 BCFTOOLS=/gsc/bin/bcftools1.2
+BGZIP=/gscmnt/gc2802/halllab/idas/software/vep/local/htslib-1.3.2/bin/bgzip
 TABIX=/gscmnt/gc2802/halllab/idas/software/vep/local/htslib-1.3.2/bin/tabix
 
 BIO_1984=/gscmnt/gc2802/halllab/idas/jira/BIO-1984
@@ -12,10 +14,40 @@ KGVCF+=ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.decompose.no
 INVCF=$1
 OUTVCF=$2
 
-${BCFTOOLS} annotate \
-    -a ${KGVCF} \
-    -c ID,INFO/1KG_EAS_AF,INFO/1KG_EUR_AF,INFO/1KG_AFR_AF,INFO/1KG_AMR_AF,INFO/1KG_SAS_AF \
-    -O z \
-    -o ${OUTVCF} \
-    ${INVCF} \
-    && ${TABIX} -p vcf -f ${OUTVCF}
+function is_empty_vcf {
+    local count=$(${ZCAT} ${INVCF} | head -n 1000 | wc -l)
+    if [[ "${count}" -gt "0" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function copy_over_vcf_headers {
+    local region=$(basename $(dirname ${INVCF}))
+    local project_path=$(dirname $(dirname $(dirname ${INVCF})))
+    local base_vcf=${project_path}/3-decompose-normalize-uniq/${region}/combined.c${region}.vcf.gz
+    ${BCFTOOLS} view -h ${base_vcf} | ${BGZIP} -c >${OUTVCF}
+    ${TABIX} ${OUTVCF}
+}
+
+function annotate_vcf {
+    ${BCFTOOLS} annotate \
+        -a ${KGVCF} \
+        -c ID,INFO/1KG_EAS_AF,INFO/1KG_EUR_AF,INFO/1KG_AFR_AF,INFO/1KG_AMR_AF,INFO/1KG_SAS_AF \
+        -O z \
+        -o ${OUTVCF} \
+        ${INVCF} \
+        && ${TABIX} -p vcf -f ${OUTVCF}
+}
+
+function main {
+    if is_empty_vcf ; then
+        echo "No variants to process copying headers over..."
+        copy_over_vcf_headers ;
+    else
+        annotate_vcf ;
+    fi
+}
+
+main ;
--- yaps2/resources/postvqsr/annotate-w-ExAC.sh
+++ yaps2/resources/postvqsr/annotate-w-ExAC.sh
@@ -12,10 +12,37 @@ KGVCF+=ExAC.r0.3.1.sites.simpleAF.multiallelic.reformatted_pop_af.vcf.gz
 INVCF=$1
 OUTVCF=$2
 
-${BCFTOOLS} annotate \
-    -a ${KGVCF} \
-    -c INFO/EXAC_AFR_AF,INFO/EXAC_AMR_AF,INFO/EXAC_Adj_AF,INFO/EXAC_EAS_AF,INFO/EXAC_FIN_AF,INFO/EXAC_NFE_AF,INFO/EXAC_OTH_AF,INFO/EXAC_SAS_AF \
-    -O z \
-    -o ${OUTVCF} \
-    ${INVCF} \
-    && ${TABIX} -p vcf -f ${OUTVCF}
+function is_empty_vcf {
+    local count=$(${BCFTOOLS} view -H ${INVCF} | head -n 1000 | wc -l)
+    if [[ "${count}" -gt "0" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function copy_over_vcf {
+    cp -v ${INVCF} ${OUTVCF}
+    cp -v ${INVCF}.tbi ${OUTVCF}.tbi
+}
+
+function annotate_vcf {
+    ${BCFTOOLS} annotate \
+        -a ${KGVCF} \
+        -c INFO/EXAC_AFR_AF,INFO/EXAC_AMR_AF,INFO/EXAC_Adj_AF,INFO/EXAC_EAS_AF,INFO/EXAC_FIN_AF,INFO/EXAC_NFE_AF,INFO/EXAC_OTH_AF,INFO/EXAC_SAS_AF \
+        -O z \
+        -o ${OUTVCF} \
+        ${INVCF} \
+        && ${TABIX} -p vcf -f ${OUTVCF}
+}
+
+function main {
+    if is_empty_vcf ; then
+        echo "No variants to process copying files over..."
+        copy_over_vcf ;
+    else
+        annotate_vcf ;
+    fi
+}
+
+main ;
