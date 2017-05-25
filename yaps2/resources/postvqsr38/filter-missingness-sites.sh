28d6b940c691dea26bfb1995976ab60cc24ca4b4
Indraniel Das <idas@wustl.edu>
2017-05-25 06:05:23 -0500

    + make steps atomic and handle empty vcf cases
      - we at least need to have the VCF header propagated so that bcftools
        stats can properly work

--- yaps2/resources/postvqsr38/filter-missingness-sites.sh
+++ yaps2/resources/postvqsr38/filter-missingness-sites.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -ueo pipefail
+
 BGZIP=/gscmnt/gc2802/halllab/idas/software/local/bin/bgzip
 TABIX=/gscmnt/gc2802/halllab/idas/software/local/bin/tabix
+BCFTOOLS=/gscmnt/gc2802/halllab/idas/software/local/bin/bcftools1.4
 
 PYTHON=$1
 SCRIPT=$2
@@ -9,12 +12,58 @@ INVCF=$3
 OUTVCF=$4
 CHROM=$5
 
-set -o xtrace
-${PYTHON} ${SCRIPT} \
-    --soft \
-    --missing-threshold=2.0 \
-    --region="${CHROM}" \
-    ${INVCF} \
-    | ${BGZIP} -c > ${OUTVCF} \
-    && ${TABIX} -p vcf -f ${OUTVCF} ;
-set +o xtrace
+function log {
+    local timestamp=$(date +"%Y-%m-%d %T")
+    echo "---> [ ${timestamp} ] $@" >&2
+}
+
+function is_empty_vcf {
+    local invcf=$1
+
+    local count=$(${BCFTOOLS} view -H ${invcf} | head -n 1000 | wc -l)
+    if [[ "${count}" -gt "0" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function copy_over_vcf {
+    local invcf=$1
+    local outvcf=$2
+
+    cp -v ${invcf} ${outvcf}
+    cp -v ${invcf}.tbi ${outvcf}.tbi
+}
+
+function filter_missingness {
+    if [ -e $OUTVCF ]
+    then
+        exit 0
+    fi
+
+    TMPVCF=$OUTVCF.temp
+
+    set -o xtrace
+    ${PYTHON} ${SCRIPT} \
+        --soft \
+        --missing-threshold=2.0 \
+        --region="${CHROM}" \
+        ${INVCF} \
+        | ${BGZIP} -c > ${TMPVCF} \
+        && ${TABIX} -p vcf -f ${TMPVCF} \
+        && mv $TMPVCF.tbi $OUTVCF.tbi \
+        && mv $TMPVCF $OUTVCF ;
+    set +o xtrace
+}
+
+function main {
+    if is_empty_vcf ${INVCF}; then
+        log "No variants to process. Copying files over..."
+        copy_over_vcf ${INVCF} ${OUTVCF} ;
+    else
+        filter_missingness ;
+    fi
+}
+
+main ;
--- yaps2/resources/postvqsr38/remove-symbolic.sh
+++ yaps2/resources/postvqsr38/remove-symbolic.sh
@@ -8,11 +8,48 @@ TABIX=/gscmnt/gc2802/halllab/idas/software/local/bin/tabix
 INVCF=$1
 OUTVCF=$2
 
-if [ -e $OUTVCF ]
-then
-    exit 0
-fi
+function log {
+    local timestamp=$(date +"%Y-%m-%d %T")
+    echo "---> [ ${timestamp} ] $@" >&2
+}
 
-TMPVCF=$OUTVCF.temp
-${BCFTOOLS} view -e '%TYPE="other" || ALT="*"' $INVCF --output-type z --output-file $TMPVCF \
-    && ${TABIX} -p vcf -f $TMPVCF && mv $TMPVCF.tbi $OUTVCF.tbi && mv $TMPVCF $OUTVCF
+function is_empty_vcf {
+    local invcf=$1
+
+    local count=$(${BCFTOOLS} view -H ${invcf} | head -n 1000 | wc -l)
+    if [[ "${count}" -gt "0" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function copy_over_vcf {
+    local invcf=$1
+    local outvcf=$2
+
+    cp -v ${invcf} ${outvcf}
+    cp -v ${invcf}.tbi ${outvcf}.tbi
+}
+
+function remove_symbolic {
+    if [ -e $OUTVCF ]
+    then
+        exit 0
+    fi
+
+    TMPVCF=$OUTVCF.temp
+    ${BCFTOOLS} view -e '%TYPE="other" || ALT="*"' $INVCF --output-type z --output-file $TMPVCF \
+        && ${TABIX} -p vcf -f $TMPVCF && mv $TMPVCF.tbi $OUTVCF.tbi && mv $TMPVCF $OUTVCF
+}
+
+function main {
+    if is_empty_vcf ${INVCF}; then
+        log "No variants to process. Copying files over..."
+        copy_over_vcf ${INVCF} ${OUTVCF} ;
+    else
+        remove_symbolic ;
+    fi
+}
+
+main ;
